aa95be1d497647163e36fce78b1cbf0d097cf6ff
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-01-04 10:43:43 +0800

    实现了在注册时，接收要注册的用户名，校验用户名是否被占用的业务

--- src/com/atguigu/schedule/controller/SysUserController.java
+++ src/com/atguigu/schedule/controller/SysUserController.java
@@ -1,9 +1,12 @@
 package com.atguigu.schedule.controller;
 
+import com.atguigu.schedule.common.Result;
+import com.atguigu.schedule.common.ResultCodeEnum;
 import com.atguigu.schedule.pojo.SysUser;
 import com.atguigu.schedule.service.SysUserService;
 import com.atguigu.schedule.service.impl.SysUserServiceImpl;
 import com.atguigu.schedule.util.MD5Util;
+import com.atguigu.schedule.util.WebUtil;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,6 +18,28 @@ import java.io.IOException;
 public class SysUserController extends BaseController {
 
 	private SysUserService userService = new SysUserServiceImpl();
+
+	/**
+	 * 注册时，接收要注册的用户名，校验用户名是否被占用的业务接口
+	 *
+	 * @param req  HttpServletRequest 对象，用于获取请求中的参数
+	 * @param resp HttpServletResponse 对象，用于发送响应
+	 */
+	protected void checkUsernameUsed(HttpServletRequest req, HttpServletResponse resp) {
+		// 接收用户名
+		String username = req.getParameter("username");
+
+		// 调用服务层业务处理方法查询该用户名是否有对应的用户
+		SysUser sysUser = userService.findByUsername(username);
+
+		Result result = Result.ok(null);
+
+		if (null != sysUser) {
+			result = Result.build(null, ResultCodeEnum.USERNAME_USED);
+		}
+		// 将result对象转换为JSON串响应给客户端
+		WebUtil.writeJson(resp, result);
+	}
 
 	/**
 	 * 接收用户登录请求的业务处理方法（业务接口）
